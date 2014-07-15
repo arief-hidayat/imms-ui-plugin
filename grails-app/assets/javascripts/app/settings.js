a8ccbf5b56c68af093cac819c59fb386b606db93
arief-hidayat <mr.arief.hidayat@gmail.com>
2014-07-15 11:25:08 +0800

    Change settings.js

--- grails-app/assets/javascripts/app/settings.js
+++ grails-app/assets/javascripts/app/settings.js
@@ -2,14 +2,14 @@
 //= require_self
 
 //App.dt.config.table = {
-////    asset : {
+////    Asset : {
 ////        columns : [ { "data": "code" }, { "data": "name" }, { "data": "assetType" }, { "data": "status" }, { "data": "locationCd" }],
 ////        order : [[1, 'asc']]
 ////    }
 //};
 //
 //App.dt.config.customUrl = {
-////        asset : {
+////        Asset : {
 ////            url : "only for custom",
 ////            extraParams : function(request) { }
 ////        }
